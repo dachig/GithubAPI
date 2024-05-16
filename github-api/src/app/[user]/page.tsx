acc1e0d1356ba1e430a8f4fe8028aeb887a7f9ea
dachi giorgobiani <dachi.giorgobiani@meteor.be>
2024-05-16 10:33:51 +0200

    chore: added react icons pacage | feat: styling repos list page using shadcn components

--- github-api/src/app/[user]/page.tsx
+++ github-api/src/app/[user]/page.tsx
@@ -1,5 +1,10 @@
+// "use client";
 import Link from "next/link";
 import { fetchUserPublicRepositories } from "../../../lib/fetchUserPublicRepositories";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { FaRegStar } from "react-icons/fa";
 
 export default async function UserPage({
   params,
@@ -7,17 +12,63 @@ export default async function UserPage({
   params: { user: string };
 }) {
   const repositories = await fetchUserPublicRepositories(params.user);
+  console.log(repositories.repositories.data[0]);
   return (
-    <main>
-      <h2 className="text-3xl font-bold tracking-tight text-primary sm:text-4xl">
-        Public repositories of {params.user}
-      </h2>
-      <div className="flex flex-col">
-        {repositories.repositories.data.map((v, index) => (
-          <Link href={`/${params.user}/${v.name}`} key={index}>
-            {v.name}
-          </Link>
-        ))}
+    <main className="relative isolate overflow-hidden py-16 sm:py-24 lg:py-32 h-screen">
+      <div className="mx-auto max-w-2xl gap-x-8 gap-y-16 px-4 lg:max-w-none">
+        <div className="max-w-xl lg:max-w-7xl mx-auto flex gap-12">
+          <div className="flex flex-col">
+            <Avatar className="w-80 h-80">
+              <AvatarImage
+                src={`${repositories.repositories.data[0].owner.avatar_url}`}
+                alt="github-avatar"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h2 className=" tracking-tight text-primary text-lg">
+              {params.user}
+            </h2>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h2 className="text-lg font-semibold tracking-tight text-primary ">
+              Public repositories of {params.user}
+            </h2>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+              {repositories.repositories.data.map((v, index) => (
+                <Card key={index} className="p-4 flex flex-col gap-4">
+                  <div className="flex justify-between">
+                    <Link
+                      href={`/${params.user}/${v.name}`}
+                      className="text-blue-600 font-semibold text-sm hover:border-b-2 hover:border-blue-600"
+                    >
+                      {v.name}
+                    </Link>
+                    <Badge className="text-gray-500" variant={"outline"}>
+                      {v.private == false ? "Public" : "Private"}
+                    </Badge>
+                  </div>
+                  <div>
+                    <p className="text-gray-500 text-sm">
+                      {v.description && v.description.length > 150
+                        ? `${v.description.substring(0, 200)}...`
+                        : v.description}
+                    </p>
+                  </div>
+                  <div className="mt-auto">
+                    {v.stargazers_count && v.stargazers_count > 0 ? (
+                      <div className="flex gap-1 items-center text-sm text-gray-500">
+                        {" "}
+                          <FaRegStar className="text-lg text-yellow-500" />{" "}
+                       {v.stargazers_count}
+                      </div>
+                    ) : null}
+                  </div>
+                </Card>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
--- github-api/src/app/components/inputUsername.tsx
+++ github-api/src/app/components/inputUsername.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -7,21 +9,20 @@ export function InputUsername() {
 
   return (
     <div className="mt-6 flex max-w-md gap-x-4">
-      <label htmlFor="github-username" className="sr-only">
-        Github username
-      </label>
-      <input
+      <Input
         id="github-username"
-        name="github-username"
         required
-        className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-        placeholder="Enter a username"
         onChange={(e) => setUsername(e.target.value)}
+        type="text"
+        placeholder="Enter a username"
+        className="bg-white/5"
       />
       <Link href={`/${username}`}>
-        <button className="flex-none rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-primary-light focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
+        <Button
+          className="px-6 text-black hover:bg-primary-foreground"
+        >
           Search
-        </button>
+        </Button>
       </Link>
     </div>
   );
